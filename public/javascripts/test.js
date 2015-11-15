e1ea63e36a0bfaa5b4e76dea0445baa73cdad725
Calvin Chan <calvin.jh.chan@gmail.com>
2015-11-15 06:12:00 -0500

    moving images

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -3,20 +3,16 @@ $(function(){
     var $dropzone=$('#dropzone')
     var mousex,mousey;
     var reader = new FileReader();
-    var lastFile=-1
     var total=[]
-    //$('#img').draggable({containment:'parent'})
+    var maxx,maxy
+    maxy=$dropzone.outerHeight()
+    maxx=$dropzone.outerWidth()
+    console.log(maxx+" "+maxy)
     function poll(){
         $.ajax({
             url:"/api/pictures",
-            //data:{'lastFile':lastFile},
             success:function(data,textStatus,jqXHR){
                 if(textStatus=="success"){
-                    //$('#img').attr('src',data)
-                    //console.log(jqXHR.getAllResponseHeaders())
-                    //var temp=jqXHR.getResponseHeader("fileName")
-                    //if(temp)
-                    //    fileName=temp
                     for(var x=0;x<data.length;x++){
                         var value=data[x]
                         if(total.indexOf(value.fileName)!=-1){
@@ -27,10 +23,20 @@ $(function(){
                             $('<img/>', {
                             id: value.fileName,
                             src: "/api/picture?fileToGet="+value.fileName,
-                            //title: 'Become a Googler',
-                            //rel: 'external',
-                            //text: 'Go to Google!'
-                        }).appendTo($dropzone).offset({top:value.x,left:value.y}).draggable({containment:'parent'});
+                            style: "position:absolute;"
+                        }).appendTo($dropzone).offset({top:value.x,left:value.y}).draggable({
+                            containment:'parent',
+                            stop:function(event,ui){
+                                $.ajax({
+                                    method:"PUT",
+                                    url:"/api/picture?fileName="+value.fileName+"&x="+ui.offset.top+"&y="+ui.offset.left
+                                })
+                            }
+                        }).on('resize',function(){
+                            var width=$(this).outerWidth();
+                            var height=$(this).outerHeight();
+                            console.log(width+" "+height)
+                        });
                         }
                     }
                 }
@@ -67,24 +73,17 @@ $(function(){
         drop(e,0)
         if(e.originalEvent.dataTransfer){
             if(e.originalEvent.dataTransfer.files.length) {
-                //console.log(e.originalEvent.dataTransfer.files);
                 f=e.originalEvent.dataTransfer.files[0]
-                reader.onload=function(dataURL) {
-                    //$('#img').attr('src',reader.result)
-
-                    reader.onload=function(arrayBuffer){
-                        $.ajax({
-                            method:"POST",
-                            url:"/api/picture/?x="+mousex+"&y="+mousey,
-                            data:arrayBuffer.target.result,
-                            processData:false,
-                            contentType:"application/binary"
-                        })
-                    }
-                    reader.readAsArrayBuffer(f);
+                reader.onload=function(arrayBuffer){
+                    $.ajax({
+                        method:"POST",
+                        url:"/api/picture/?x="+mousex+"&y="+mousey,
+                        data:arrayBuffer.target.result,
+                        processData:false,
+                        contentType:"application/binary",
+                    })
                 }
-                reader.readAsDataURL(f);
-
+                reader.readAsArrayBuffer(f);
             }
         }
     })
