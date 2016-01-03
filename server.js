96c8075555e2c7e85370881f516af5f59c3ee883
Justin Stribling <jastribl@uwaterloo.ca>
2016-01-03 13:05:10 -0500

    added jquery-ui-touch-punch for use with touch screens

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ app.use(express["static"](__dirname + '/node_modules/dropzone/dist'));
 
 app.use(express["static"](__dirname + '/node_modules/angular-route'));
 
+app.use(express["static"](__dirname + '/node_modules/jquery-ui-touch-punch'));
+
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 
 app.use(session({
